460e288a31e570e6796393b9c65061524cc7ca35
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-09-12 21:37:47 +0530

    add new question

---
+++ 09_if_else/question4.java
@@ -0,0 +1,41 @@
+// Write a program in java to make a simple calculator using if else statement.
+
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter First Number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter Operator: ");
+        char operator = sc.next().charAt(0);
+
+        double result = 0;
+
+          if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+                sc.close();
+                return;
+            }
+        } else {
+            System.out.println("Error: Invalid operator.");
+            sc.close();
+            return;
+        }
+        System.out.println("The result is: " + result);
+        sc.close();
+    }
+}
