b91fc9721ff9578d4ef66c85457ca315d7a92e3e
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-09-15 00:28:18 +0530

    explain while_loop

---
+++ 10_Loop/while_loop/question1.java
@@ -0,0 +1,10 @@
+public class question1 {
+    public static void main(String args[]){
+        
+        int i = 0; //initialization
+        while(i<11){ //condition
+            System.out.println(i);
+            i++; //increment
+        }
+    }
+}
