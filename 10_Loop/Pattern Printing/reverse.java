a483c17c03ee46477780f966d08b91d0b7dffcdf
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-09-17 22:06:10 +0530

    add new question

--- 10_Loop/Pattern Printing/reverse.java
+++ 10_Loop/Pattern Printing/reverse.java
@@ -1,4 +1,12 @@
-
+//Pattern Problems Reverse a number
 public class reverse {
-    
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
