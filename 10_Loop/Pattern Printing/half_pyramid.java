b08da8e9e5590574f951adf02b427adb38bfd64b
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-09-18 19:47:54 +0530

    add new question

--- 10_Loop/Pattern Printing/half_pyramid.java
+++ 10_Loop/Pattern Printing/half_pyramid.java
@@ -10,11 +10,30 @@
 //     }
 // }
 
-//inverted half pyramid
-public class half_pyramid {
+//2. inverted half pyramid
+// public class half_pyramid {
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=n; i>=1; i--){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//3. inverted half pyramid rotated by 180 degree
+
+ public class half_pyramid {
     public static void main(String[] args){
         int n = 5;
-        for(int i=n; i>=1; i--){
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            //stars
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
