72f796183a753f72131fedad30e384fffd7beb7d
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-09-19 22:01:56 +0530

    fix question

--- 10_Loop/Pattern Printing/halfPyramidNumber.java
+++ 10_Loop/Pattern Printing/halfPyramidNumber.java
@@ -3,12 +3,8 @@ public class halfPyramidNumber {
         int n = 5;
         for(int i=1; i<=n; i++){
             //spaces
-            for(int j=1; j<=n-i; j++){
-                System.out.print("  ");
-            }
-            //numbers
-            for(int j=1; j<=i; j++){
-                System.out.print(i + " ");
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
