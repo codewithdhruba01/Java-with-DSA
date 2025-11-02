01b0d8489358d08c050c3d59364f2f1bd1dd6b0d
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-02 22:20:22 +0530

    update question

--- 11_Function/Factorial.java
+++ 11_Function/Factorial.java
@@ -1,21 +1,18 @@
-
 import java.util.*;
-
 
 public class Factorial {
     public static void  calculateFactorial(int n) { // Function Definition
         int factorial = 1;
-        for (int i = n; i >= 1; i--) {
+        for (int i = n; i >= 1; i--) { // Loop to calculate factorial
             factorial = factorial * i;
         }
-        System.out.println("Factorial is: " + factorial);
+        System.out.println("Factorial is: " + factorial); // Print Result
     }   
 
     public static void main(String args[]) { // Main Method
         Scanner sc = new Scanner(System.in); // Scanner Object
         System.out.print("Enter a number to find its factorial: "); 
-        int n = sc.nextInt();
-
-       
+        int n = sc.nextInt(); // Input Number
+        calculateFactorial(n); // Function Call
     }
 }
