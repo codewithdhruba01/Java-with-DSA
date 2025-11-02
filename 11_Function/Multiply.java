5219af8209d4ca75ff476a5b41b7a8c1a3934ec5
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-02 21:51:53 +0530

    add new practice question

--- 11_Function/Multiply.java
+++ 11_Function/Multiply.java
@@ -1,3 +1,19 @@
+//Make a function to multiply two numbers and return the product
+import java.util.*;
+
 public class Multiply {
+    public static int calculateProduct(int a, int b) { // Function Definition
+        return a * b; // Return the product directly
+    }   
+
+    public static void main(String args[]) { // Main Method
+        Scanner sc = new Scanner(System.in); // Scanner Object
+        System.out.print("Enter First numbers: "); 
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Product of 2 Number: " + calculateProduct(a, b) ); // Function Call and print
+    }
     
 }
