834a2f19f2ea1ec0adea31595a3db21640c395d6
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-02 21:37:07 +0530

    add practice question

---
+++ 11_Function/Sum.java
@@ -0,0 +1,20 @@
+//Make a function to add two numbers and return the sum
+import java.util.*;
+
+public class Sum {
+    public static int calculateSum(int a, int b) { // Function Definition
+        int sum = a + b;
+        return sum;
+    }   
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First numbers: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b);
+        System.out.println("Sum is 2 Number: " + sum);
+    }
+}
