142a33b3deb675f7d8d3bcdfb7b992b59184e8f5
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-02 22:05:59 +0530

    update file path

---
+++ 11_Function/Practice Code/Factorial.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+
+public class Factorial {
+    public static void  calculateFactorial(int n) { // Function Definition
+        int factorial = 1;
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial is: " + factorial);
+        return;
+    }   
+
+    public static void main(String args[]) { // Main Method
+        Scanner sc = new Scanner(System.in); // Scanner Object
+        System.out.print("Enter a number to find its factorial: "); 
+        int n = sc.nextInt();
+
+       
+    }
+}
