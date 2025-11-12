94f8ca0438ebe73cb0a906a50acc4afa2415a97e
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-12 18:07:30 +0530

    add new practice set

---
+++ 07_String/Concatenation.java
@@ -0,0 +1,8 @@
+public class Concatenation {
+    public static void main(String args[]) {
+        String firstName = "Dhrubaraj";
+        String lastName = "Pati";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
+}
