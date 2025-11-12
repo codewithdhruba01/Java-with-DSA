54aaad986eb29fec243c65fcd3f6e05b8f16b737
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-12 18:14:40 +0530

    update

--- 07_String/Length.java
+++ 07_String/Length.java
@@ -2,5 +2,10 @@ public class Length {
     public static void main(String args[]) {
         String name = "Dhrubaraj";
         System.out.println(name.length());
+
+        //charAt()
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
     }
 }
